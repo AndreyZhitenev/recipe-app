251cc6a41e298bd7a2db9017000ef2fac2badebd
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-06-19 05:07:53 +0300

    added meal info in popup

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const mealsEl = document.getElementById('meals')
 const favoriteContainer = document.getElementById('fav-meals')
+const mealPopup = document.getElementById('meal-popup')
+const mealInfoEl = document.getElementById('meal-info')
+const popupCloseBtn = document.getElementById('close-popup')
 
 const searchTerm = document.getElementById('search-term')
 const searchBtn = document.getElementById('search')
@@ -70,6 +73,10 @@ function addMeal(mealData, random = false) {
 		fetchFavMeals()
 	})
 
+	meal.addEventListener('click', () => {
+		showMealInfo(mealData)
+	})
+
 	mealsEl.appendChild(meal)
 }
 
@@ -122,7 +129,52 @@ function addMealFav(mealData) {
 		fetchFavMeals()
 	})
 
+	favMeal.addEventListener('click', () => {
+		showMealInfo(mealData)
+	})
+
 	favoriteContainer.appendChild(favMeal)
+}
+
+function showMealInfo(mealData) {
+	// clean it up
+	mealInfoEl.innerHTML = ''
+
+	// update the Meal info
+	const mealEl = document.createElement('div')
+
+	const ingredients = []
+	// get ingredients and measures
+	for (let i = 1; i <= 20; i++) {
+		if (mealData['strIngredient' + i]) {
+			ingredients.push(
+				`${mealData['strIngredient' + i]} - ${mealData['strMeasure' + i]}`
+			)
+		} else {
+			break
+		}
+	}
+
+	mealEl.innerHTML = `
+		<h1>${mealData.strMeal}</h1>
+		<img
+			src="${mealData.strMealThumb}"
+			alt="${mealData.strMeal}"
+		/>
+
+		<p>
+			${mealData.strInstructions}
+		</p>
+		<h3>Ingredients:</h3>
+		<ul>
+		${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+		</ul>
+		`
+
+	mealInfoEl.appendChild(mealEl)
+
+	// show the popup
+	mealPopup.classList.remove('hidden')
 }
 
 searchBtn.addEventListener('click', async () => {
@@ -137,4 +189,8 @@ searchBtn.addEventListener('click', async () => {
 			addMeal(meal)
 		})
 	}
+})
+
+popupCloseBtn.addEventListener('click', async () => {
+	mealPopup.classList.add('hidden')
 })
